95d12e696a859157b1a7e6b78478658eeea7cf8a
Md Moinuddin <noobycox@proton.me>
2024-07-28 21:38:10 +0530

    testing env

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <main className="text-4xl font-bold text-center">
       <h1>Thunderbolt</h1>
+
+      <h1>{process.env.VERCEL_ENV}</h1>
+      <h1>{process.env.ENV}</h1>
     </main>
   );
 }
